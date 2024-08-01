d98a46ce9c26823ed96326a3fcadc3ceab8de35a
Thannia Hernandez <thaherna1@gmail.com>
2024-08-01 09:16:44 -0500

    added code to add install availibility

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,25 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const ev = window.deferredPrompt;
+    if(ev) {
+        ev.prompt();
+        window.deferredPrompt = null;
+        butInstall.classList.toggle('hidden', true);
+    } else {
+        return;
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
