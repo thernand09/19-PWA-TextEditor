52ddb329e27d059beedcffa82c21f8ea8f0801d6
Thannia Hernandez <thaherna1@gmail.com>
2024-08-01 09:12:31 -0500

    added databs.js file code, to initiate db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+
+  const transaction = db. transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+
+  // return 0 is empty
+
+  const count = (await obStore.get('counter')) || 0;
+  count++
+  const result = await obStore.put({ id: count, value: content });
+
+  await transaction.done;
+
+  return result;
+
+  // console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
 
+  const transaction = db.transaction('jate', 'readwrite');
+  const obStore = transaction.objectStore('jate');
+
+  const result = await obStore.getAll();
+
+  await transaction.done;
+
+  return result.value;
+  
+  //console.error('getDb not implemented');
+}
 initdb();
